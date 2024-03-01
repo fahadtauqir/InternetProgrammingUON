cff89709c4263875ba59cf1c9f607c59fcdcaa85
fahadtauqir <fahad_tauqir@yahoo.com>
2024-03-01 13:44:11 +0000

    week5

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-function print5() {
-    for (let i = 1; i <= 5; i++) {
-        console.log(i);
-    }
-}
-
-// Add event listener for the 'DOMContentLoaded' event
 document.addEventListener('DOMContentLoaded', function() {
-    // Update the content of the <h1> element
-    document.getElementById('myHeading').textContent = 'Updated Heading Content';
-
-    // Update the content of the <p> element
-    document.getElementById('myParagraph').textContent = 'Updated paragraph content.';
-
-    // Call print5 twice
-    print5();
-    print5();
+    // Get the element with the ID 'circle'
+    var circle = document.getElementById('circle');
+    
+    // Add a click event listener to the 'circle' element
+    circle.addEventListener('click', function() {
+        // Display an alert box when the 'circle' is clicked
+        alert('Button is pressed');
+    });
 });
